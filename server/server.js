95e7629d13ae3a32f083175f49f34f386d854c2b
Mohammed Anas Javeed <anasjaveed@users.noreply.github.com>
2020-06-15 03:15:15 +0530

    changes in json and sever

--- server/server.js
+++ server/server.js
@@ -21,17 +21,21 @@ mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE)
+mongoose.connect(process.env.MONGODB_URI)
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'))
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 })
+
+
 
 // STORAGE MULTER CONFIG
 
@@ -564,6 +568,14 @@ app.post("/api/site/site_data",auth,admin,(req,res)=>{
         }
     )
 })
+// DEFAULT 
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
+
 
 
 const port = process.env.PORT || 3002;
