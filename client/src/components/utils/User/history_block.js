c5c6e4eb3f8c83a7a208e261a52d7033f7b15f4b
Mohammed Anas Javeed <anasjaveed@users.noreply.github.com>
2020-06-15 00:07:07 +0530

    initial commit

---
+++ client/src/components/utils/User/history_block.js
@@ -0,0 +1,44 @@
+import React from "react";
+// import moment from "moment";
+/* <td>{moment(product.dateOfPurchase).format("MM-DD-YYYY")}</td> */
+
+
+const UserHistoryBlock = (props)=>{
+
+const renderBlocks =()=>(
+    props.products  ?
+       props.products.map((product,i)=>(
+           <tr key={i}>
+               <td>{product.porder}</td>
+               <td>{product.brand} {product.name}</td>
+                <td>₹ {product.price}</td>
+                <td>{product.quantity}</td>
+           </tr>
+))
+    
+    :null
+
+)
+
+
+    return(
+        <div className="history_blocks">
+        <table>
+            <thead>
+                <tr>
+                    <th> Order Number</th>
+                    <th> Product</th>
+                    <th> Price Paid</th>
+                    <th> Quantity </th>
+                    </tr>
+            </thead>
+            <tbody>
+                {renderBlocks()}
+            </tbody>
+        </table>
+                  
+        </div>
+    );
+};
+
+export default UserHistoryBlock;
